458729fd2d3dc25910cbdadc0da17ff5c45bbbe4
duno <kingalbot@mail.ru>
2020-06-20 23:57:02 +0300

    release 1.0

--- getPoems.py
+++ getPoems.py
@@ -1,8 +1,11 @@
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
+import re
 
+def souper():
+    pass
 
-url = 'http://stih.su/' 
+url = 'http://stih.su' 
 resp = urlopen(url) # скачиваем файл
 html = resp.read().decode('utf8') # считываем содержимое
 soup = BeautifulSoup(html, 'html.parser') # делаем суп
@@ -18,7 +21,7 @@ for i in range(len(authors_lst)):
         break
 
 poems_links = []
-for author in authors_lst:
+for author in authors_lst[0:2]:
     resp = urlopen(url+author) 
     html = resp.read().decode('utf8')
     soup = BeautifulSoup(html, 'html.parser')
@@ -26,9 +29,26 @@ for author in authors_lst:
     poems_div = soup.find('ol', {'class': 'number-navi all'}) 
     poems_tags = list(poems_div.find_all("a"))
     links = [a.attrs['href'] for a in poems_tags]
-    poems_links.append(links)
+    poems_links += links
 
-print(poems_links)
+count = 1
+poems_path = "poemsData"
+for poem_link in poems_links:
+    resp = urlopen(poem_link)
+    html = resp.read().decode('utf8')
+    soup = BeautifulSoup(html, 'html.parser')
 
+    poem_title = soup.find('h1', {'class': 'entry-title'})
+    poem_title = poem_title.next_element
+    print(f'{count}:', poem_title)
 
+    poem = soup.find('div', {'class': 'entry-content'})
+    poem_lines = list(poem)[3:-1]
 
+    poemsPath = "poemsData"
+    path = f'{poemsPath}/{poem_title}.txt'
+    with open(path, 'w', encoding='utf8') as file:
+        for l in poem_lines:
+            string = re.sub(r"<p>|<br\/>|<\/p>", "", str(l))
+            file.write(string+'\n')
+    count += 1 
